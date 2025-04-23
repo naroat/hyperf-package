2cb1a593ef7f85a411f3c6bfce99e26684ddc135
taoran <taoran0796@163.com>
2025-04-23 15:40:59 +0800

    feat: add genModule

---
+++ src/Command/GenModule.php
@@ -0,0 +1,58 @@
+<?php
+namespace Naroat\HyperfPackage\Command;
+
+use Hyperf\Command\Command as HyperfCommand;
+use Hyperf\Command\Annotation\Command;
+use Symfony\Component\Console\Input\InputArgument;
+
+#[Command]
+class GenModule extends HyperfCommand
+{
+    public function __construct()
+    {
+        parent::__construct('gen:module');
+        $this->setDescription('Create new Module.');
+    }
+
+    public function configure()
+    {
+        parent::configure();
+        $this->addArgument('name', InputArgument::REQUIRED, 'module name');
+    }
+
+    public function handle()
+    {
+        $name = $this->input->getArgument('name');
+
+        //path
+        $path = BASE_PATH . '/app/' . $name;
+
+        //create dir
+        $dirs = [
+            'Annotation',
+            'Aspect',
+            'Cache',
+            'Command',
+            'Controller',
+            'Event',
+            'Listener',
+            'Middleware',
+            'Model',
+            'Request',
+            'Service',
+        ];
+
+        if (file_exists($path) && is_dir($path)) {
+            $this->warn('The directory exists.');
+        }
+
+        foreach ($dirs as $dir) {
+            $pathFull = $path . '/' . $dir;
+            @mkdir($pathFull, 0755, true);
+        }
+
+        $this->line('ok', 'info');
+    }
+
+
+}
---
+++ tests/CommandTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Naroat\tests;
+
+use Hyperf\Command\Console;
+use Naroat\HyperfPackage\Command\GenModule;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\NullOutput;
+
+class CommandTest extends TestCase
+{
+    public function testDemo()
+    {
+
+    }
+}
